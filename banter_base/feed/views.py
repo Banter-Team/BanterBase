1a5ba7ddbf6ba9dd25b27c574d810e2c4aee9e23
Paul Gralla <paulg@Pauls-MacBook-Pro-5.local>
2020-04-05 17:09:03 -0400

    Use Django forms instead

--- banter_base/feed/views.py
+++ banter_base/feed/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import BanterEntry
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def home(request):
@@ -6,4 +8,18 @@ def home(request):
 
 
 def new_entry(request):
-        return render(request, 'feed/new_entry.html')
+           # if this is a POST request we need to process the form data
+        if request.method == 'POST':
+                # create a form instance and populate it with data from the request:
+                form = BanterEntry(request.POST)
+                # check whether it's valid:
+                if form.is_valid():
+                        # process the data in form.cleaned_data as required
+                        # ...
+                        # redirect to a new URL:
+                        return HttpResponseRedirect('/thanks/')
+
+                # if a GET (or any other method) we'll create a blank form
+        else:
+                form = BanterEntry()
+        return render(request, 'feed/new_entry.html', {'form':form})
